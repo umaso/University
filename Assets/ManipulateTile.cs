8b230368c4d7a5b19459c3ca001abdee20a850e4
Zephyr Zhang <zephyr1125@gmail.com>
2017-02-12 21:04:23 +0800

    初次导入

---
+++ Assets/ManipulateTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ManipulateTile : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap _wallTileMap, _groundTileMap;
+    [SerializeField]
+    private TilemapRenderer _wallTileMapRenderer, _groundTileMapRenderer;
+
+    [SerializeField] private TileBase _groundTileBase;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyUp(KeyCode.N))
+	    {
+	        Debug.Log("New Tile!");
+            _groundTileMap.SetTile(new Vector3Int(Random.Range(-10, 10), Random.Range(-5, 5), 0), _groundTileBase);
+        }
+	}
+}
