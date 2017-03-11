33e3ab71753ff33dba9fe65239de5ea9e6747ec3
Zephyr Zhang <zephyr1125@gmail.com>
2017-03-11 18:45:38 +0800

    config cursor

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    public Sprite CursorSprite;
+
+	// Use this for initialization
+	void Awake () {
+		//Cursor.SetCursor(CursorSprite.texture, Vector2.zero , CursorMode.Auto);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
