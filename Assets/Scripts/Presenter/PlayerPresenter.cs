86478dd8095b7e3b10d222ce4b9b1e69994b2640
Zephyr Zhang <zephyr1125@gmail.com>
2017-03-19 17:34:42 +0800

    实现鼠标拖拽规划地块

--- Assets/Scripts/Presenter/PlayerPresenter.cs
+++ Assets/Scripts/Presenter/PlayerPresenter.cs
@@ -29,7 +29,9 @@ namespace Zephyr.University.Presenter
 
         private Vector3Int _cellPlanningStart, _cellPlanningEnd;
 
-        public Tilemap GroundTilemap;
+        public Tilemap GroundTilemap, GroundPreviewLayer;
+
+        public TileBase GroundPreviewTile;
 
         /// <summary>
         /// 光标当前所在的Cell
@@ -37,6 +39,11 @@ namespace Zephyr.University.Presenter
         public Vector3Int CurrentCell;
 
         private Vector3Int _newCell;
+
+        /// <summary>
+        /// 地表预览的绘制cell列表，玩家移动鼠标时更新
+        /// </summary>
+        private List<Vector3Int> _groundPreviewCells = new List<Vector3Int>();
 
         // Use this for initialization
         private void Start()
@@ -46,8 +53,7 @@ namespace Zephyr.University.Presenter
                 .Subscribe(l =>
             {
                 //行为：更新光标位置
-                Vector2 mouseInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                CurrentCell = GroundTilemap.WorldToCell(mouseInWorld);
+                CurrentCell = Utils.GetMouseCell(GroundTilemap);
             });
 
             //按下建设按钮就进入规划状态
@@ -56,34 +62,72 @@ namespace Zephyr.University.Presenter
                 CurrentOperateState = OperateState.Planning;
             });
 
-            //规划状态下右键点击进入待机状态
+            //规划状态下的各种处理
             Observable.EveryUpdate()
                 .Where(l => CurrentOperateState == OperateState.Planning)
-                .Where(l => Input.GetMouseButtonUp(1))
                 .Subscribe(_ =>
-                {
-                    CurrentOperateState = OperateState.Idle;
-                });
+                { 
+                    if (Input.GetMouseButtonUp(1))
+                    {
+                        //右键点击回到待机状态
+                        ClearGroundPreviewCells();
+                        CurrentOperateState = OperateState.Idle;
+                    }else if (!Input.GetMouseButton(0))
+                    {
+                        //单一块预览(鼠标没按下时)
+                        var mouseCell = Utils.GetMouseCell(GroundTilemap);
+                        UpdateGroundPreviewCells(mouseCell, mouseCell);
+                    }else if (Input.GetMouseButtonDown(0))
+                    {
+                        //左键点下时记录起始tile
+                        _cellPlanningStart = CurrentCell;
+                    }else if (Input.GetMouseButton(0))
+                    {
+                        //按住时获取结束tile
+                        _cellPlanningEnd = CurrentCell;
+                        //基于起始位置更新绘制区方块
+                        UpdateGroundPreviewCells(_cellPlanningStart, _cellPlanningEnd);
+                    }
 
-            //规划状态下获取左键点下时的起始tile
-            Observable.EveryUpdate()
-                .Where(l => CurrentOperateState == OperateState.Planning)
-                .Where(l => Input.GetMouseButtonDown(0))
-                .Subscribe(_ =>
-                {
-                    _cellPlanningStart = CurrentCell;
-                    Debug.Log("Start = "+_cellPlanningStart);
+                    //地表预览层更新要绘制的cell
+                    GroundPreviewLayer.ClearAllTiles();
+                    foreach (var cell in _groundPreviewCells)
+                    {
+                        GroundPreviewLayer.SetTile(cell, GroundPreviewTile);
+                    }
                 });
+        }
 
-            //规划状态下获取左键抬起时的结束tile
-            Observable.EveryUpdate()
-                .Where(l => CurrentOperateState == OperateState.Planning)
-                .Where(l => Input.GetMouseButtonUp(0))
-                .Subscribe(_ =>
+        /// <summary>
+        /// 清空地表预览区
+        /// </summary>
+        private void ClearGroundPreviewCells()
+        {
+            _groundPreviewCells.Clear();
+        }
+
+        /// <summary>
+        /// 更新地表预览区要绘制的cell数组
+        /// </summary>
+        /// <param name="startCell"></param>
+        /// <param name="endCell"></param>
+        private void UpdateGroundPreviewCells(Vector3Int startCell, Vector3Int endCell)
+        {
+            ClearGroundPreviewCells();
+
+            int minX = Math.Min(startCell.x, endCell.x);
+            int maxX = Math.Max(startCell.x, endCell.x);
+
+            int minY = Math.Min(startCell.y, endCell.y);
+            int maxY = Math.Max(startCell.y, endCell.y);
+
+            for (int i = minX; i <= maxX; i++)
+            {
+                for (int j = minY; j <= maxY; j++)
                 {
-                    _cellPlanningEnd = CurrentCell;
-                    Debug.Log("End = " + _cellPlanningEnd);
-                });
+                    _groundPreviewCells.Add(new Vector3Int(i, j, 0));
+                }
+            }
         }
     }
 }
--- Assets/Scripts/Presenter/Utils.cs
+++ Assets/Scripts/Presenter/Utils.cs
@@ -5,6 +5,10 @@ namespace Zephyr.University.Presenter
 {
     public class Utils
     {
-
+        public static Vector3Int GetMouseCell(Tilemap inTileMap)
+        {
+            Vector2 mouseInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return inTileMap.WorldToCell(mouseInWorld);
+        }
     }
 }
